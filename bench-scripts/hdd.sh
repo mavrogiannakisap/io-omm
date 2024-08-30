c381ab3807d78dacbd9719b123438e40de7b384b
Ubuntu <apostolos@hdd-heavy.s0e2l0t1sx5ubmqkq2cobn3luh.phxx.internal.cloudapp.net>
2024-08-30 02:50:24 +0000

    updating hdd.sh

--- bench-scripts/hdd.sh
+++ bench-scripts/hdd.sh
@@ -10,7 +10,7 @@ for v in ${vs[@]}; do
   mkdir -p ${hdd_base_path}
   out="hdd-ad-v${v}-s${s}-lf${lf}-lvl${lvl}-ofs-res.csv"
   if [ "$s" -eq 1 ]; then
-    test -s ${out} || ${ofs_bench_ex} --print_csv_headers -r ${num_runs} -v ${v} -N ${n} -s ${s} -L ${lf} -p ${hdd_base_path} -d >${out}
+    test -s ${out} || ${ofs_bench_ex} --print_csv_headers -r ${num_runs} -v ${v} -N ${n} -s ${s} -L ${lf} -p ${hdd_base_path} -i ${lvl} -d >${out}
   else
     test -s ${out} || ${ofs_multi_bench_ex} --print_csv_headers -r ${num_runs} -v ${v} -N ${n} -s ${s} -L ${lf} -p ${hdd_base_path} -d -i ${lvl} >${out}
   fi
